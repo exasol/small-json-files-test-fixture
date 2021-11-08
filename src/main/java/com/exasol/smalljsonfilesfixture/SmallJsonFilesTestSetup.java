8929a6415c665e02f32615c42c1a8e9a625b9be9
Jakob Braun <jakob.braun@posteo.de>
2021-11-08 05:42:37 -0400

    #6: Only install lambda if needed #3: Prefix resources to avoid collisions

---
+++ src/main/java/com/exasol/smalljsonfilesfixture/SmallJsonFilesTestSetup.java
@@ -0,0 +1,106 @@
+package com.exasol.smalljsonfilesfixture;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.*;
+import java.util.logging.Logger;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+
+/**
+ * This class creates a test setup with small JSON files on AWS S3. Since creating is slow and expensive this class
+ * checks if the setup is already there before creating it.
+ */
+public class SmallJsonFilesTestSetup {
+    private static final String CREATE_JSON_FILES_LAMBDA = "createJsonFilesLambda/createJsonFilesLambda.js";
+    private static final Logger LOGGER = Logger.getLogger(SmallJsonFilesTestSetup.class.getName());
+    private static final String SETUP_DESCRIPTION_KEY = ".setup-description.json";
+
+    /**
+     * Create the test fixture.
+     *
+     * @param tags                tags for the AWS resources
+     * @param bucket              s3-bucket
+     * @param credentialsProvider AWS credentials provider
+     * @throws IOException if something goes wrong
+     */
+    public void setup(final Map<String, String> tags, final String bucket,
+            final AwsCredentialsProvider credentialsProvider, final int numberOfJsonFiles, final int filesPerLambda)
+            throws IOException {
+        final TestSetupDescription testSetupDescription = new TestSetupDescription(numberOfJsonFiles,
+                getLambdaFunctionHash());
+        if (isCached(testSetupDescription, credentialsProvider, bucket)) {
+            LOGGER.info("Test setup is already in-place");
+        } else {
+            try (final S3TestSetupLambdaController controller = S3TestSetupLambdaController.create(tags, bucket,
+                    credentialsProvider)) {
+                controller.deleteFiles();
+                controller.createFiles(numberOfJsonFiles, filesPerLambda);
+                uploadSetupDescription(testSetupDescription, credentialsProvider, bucket);
+            }
+        }
+    }
+
+    private void uploadSetupDescription(final TestSetupDescription testSetupDescription,
+            final AwsCredentialsProvider credentialsProvider, final String bucket) {
+        try (final S3Client s3Client = createS3Client(credentialsProvider)) {
+            s3Client.putObject(request -> request.bucket(bucket).key(SETUP_DESCRIPTION_KEY),
+                    RequestBody.fromString(testSetupDescription.toJson()));
+        }
+    }
+
+    private boolean isCached(final TestSetupDescription requestedSetup,
+            final AwsCredentialsProvider credentialsProvider, final String bucket) {
+        final Optional<TestSetupDescription> actualSetup = fetchSetupDescription(credentialsProvider, bucket);
+        if (actualSetup.isEmpty()) {
+            return false;
+        } else {
+            return actualSetup.get().equals(requestedSetup);
+        }
+    }
+
+    private String getLambdaFunctionHash() {
+        try {
+            final var hashBuilder = MessageDigest.getInstance("SHA-256");
+            try (final InputStream lambdaAsStream = Objects
+                    .requireNonNull(getClass().getClassLoader().getResourceAsStream(CREATE_JSON_FILES_LAMBDA))) {
+                final byte[] byteArray = new byte[1024];
+                int readBytes = 0;
+                while ((readBytes = lambdaAsStream.read(byteArray)) != -1) {
+                    hashBuilder.update(byteArray, 0, readBytes);
+                }
+            }
+            final byte[] hash = hashBuilder.digest();
+            final StringBuilder hashStringBuilder = new StringBuilder();
+            for (final byte hashByte : hash) {
+                hashStringBuilder.append(Integer.toString((hashByte & 0xff) + 0x100, 16).substring(1));
+            }
+            return hashStringBuilder.toString();
+        } catch (final Exception exception) {
+            throw new IllegalStateException("Failed to build hash-code of lambda function", exception);
+        }
+    }
+
+    private Optional<TestSetupDescription> fetchSetupDescription(final AwsCredentialsProvider credentialsProvider,
+            final String bucket) {
+        try (final S3Client s3Client = createS3Client(credentialsProvider)) {
+            final String json = new String(
+                    s3Client.getObject(request -> request.bucket(bucket).key(SETUP_DESCRIPTION_KEY)).readAllBytes(),
+                    StandardCharsets.UTF_8);
+            return Optional.of(TestSetupDescription.fromJson(json));
+        } catch (final NoSuchKeyException exception) {
+            return Optional.empty();
+        } catch (final IOException exception) {
+            throw new IllegalStateException("Failed to fetch setup description.", exception);
+        }
+    }
+
+    private S3Client createS3Client(final AwsCredentialsProvider credentialsProvider) {
+        return S3Client.builder().credentialsProvider(credentialsProvider).build();
+    }
+}
--- src/test/java/com/exasol/smalljsonfilesfixture/S3TestSetupLambdaControllerIT.java
+++ src/test/java/com/exasol/smalljsonfilesfixture/S3TestSetupLambdaControllerIT.java
@@ -1,9 +1,9 @@
 package com.exasol.smalljsonfilesfixture;
 
+import static com.exasol.smalljsonfilesfixture.S3TestUtils.countDataFiles;
+import static com.exasol.smalljsonfilesfixture.S3TestUtils.emptyS3Bucket;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.lessThan;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
@@ -13,8 +13,6 @@ import org.junit.jupiter.api.*;
 
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
 
 class S3TestSetupLambdaControllerIT {
     private static final TestConfig TEST_CONFIG = TestConfig.instance();
@@ -40,35 +38,14 @@ class S3TestSetupLambdaControllerIT {
 
     @AfterEach
     void afterEach() {
-        emptyS3Bucket(bucketName);
+        emptyS3Bucket(bucketName, s3Client);
     }
 
     @Test
     void testCreateFiles() {
         final int fileCount = 100;
         controller.createFiles(fileCount, 10);
-        assertThat(countDataFiles(), equalTo(fileCount));
-    }
-
-    @Test
-    void testCreateFileWithCache() {
-        final int fileCount = 100;
-        controller.createFiles(fileCount, 10);
-        final long start = System.currentTimeMillis();
-        controller.createFiles(fileCount, 10);
-        final int elapsed = (int) (System.currentTimeMillis() - start);
-        assertAll(//
-                () -> assertThat(elapsed, lessThan(1000)), //
-                () -> assertThat(countDataFiles(), equalTo(fileCount))//
-        );
-    }
-
-    @Test
-    void testInvalidateCache() {
-        controller.createFiles(100, 10);
-        final int newFileCount = 10;
-        controller.createFiles(newFileCount, 10);
-        assertThat(countDataFiles(), equalTo(newFileCount));
+        assertThat(countDataFiles(s3Client, bucketName), equalTo(fileCount));
     }
 
     @Test
@@ -76,7 +53,7 @@ class S3TestSetupLambdaControllerIT {
         s3Client.putObject(request -> request.bucket(bucketName).key("test-data-1.json"),
                 RequestBody.fromString("myObject"));
         controller.deleteFiles();
-        assertThat("the file was not deleted", countDataFiles(), equalTo(0));
+        assertThat("the file was not deleted", countDataFiles(s3Client, bucketName), equalTo(0));
     }
 
     @Test
@@ -91,22 +68,5 @@ class S3TestSetupLambdaControllerIT {
         final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> controller.createFiles(10, 3));
         assertThat(exception.getMessage(), equalTo("Number of JSON files must be a multiple of filesPerLambda(3)."));
-    }
-
-    private void emptyS3Bucket(final String bucketName) {
-        final ListObjectsV2Iterable pages = s3Client.listObjectsV2Paginator(request -> request.bucket(bucketName));
-        for (final ListObjectsV2Response page : pages) {
-            page.contents().forEach(
-                    s3Object -> s3Client.deleteObject(builder -> builder.bucket(bucketName).key(s3Object.key())));
-        }
-    }
-
-    private int countDataFiles() {
-        final var pages = s3Client.listObjectsV2Paginator(request -> request.bucket(bucketName).prefix("test-data"));
-        int objectCounter = 0;
-        for (final ListObjectsV2Response page : pages) {
-            objectCounter += page.contents().size();
-        }
-        return objectCounter;
     }
 }
