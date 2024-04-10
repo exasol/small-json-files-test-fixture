adc53ae4b647f4c9e362c174a8368e7cd4d505ea
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-04-10 15:13:27 +0200

    Cleanup eslint config

--- src/main/resources/createJsonFilesLambda/eslint.config.js
+++ src/main/resources/createJsonFilesLambda/eslint.config.js
@@ -2,13 +2,8 @@ import globals from 'globals'
 
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+const compat = new FlatCompat({ recommendedConfig: pluginJs.configs.recommended })
 
 export default [
     { languageOptions: { globals: globals.browser } },
